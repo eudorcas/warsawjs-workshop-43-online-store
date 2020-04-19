b0586719456cef303e69e126aeaff3bf6fa5afd8
eudorcas <klaudia.butkiewicz09@gmail.com>
2020-04-19 11:18:37 +0200

    Create classes and bootstrap

---
+++ src/components/cart.js
@@ -0,0 +1,19 @@
+import { Component } from '../components/component';
+
+export class Cart extends Component {
+    constructor() {
+        super();
+        this.list = [];
+    }
+    get template() {
+        return `<div class="cart">
+        <button class="btn btn-primary">
+            Cart (XXX)
+        </button>
+    </div>`;
+    }
+
+    addProduct(product) {
+        this.list.push(product);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import './index.html';
+import { Cart } from './components/cart';
+import { Product } from './components/product';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log('hello');
+
+function main() {
+    const $toolbar = document.querySelector('.toolbar');
+    const $products = document.querySelector('.products');
+    const c = new Cart();
+    c.render($toolbar);
+    const p = new Product();
+    p.render($products);
+}
+
+main();
